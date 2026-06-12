d203cc174e93fbe3f4c41f7411970e6f5cadf577
kayla <koala>
2026-06-12 15:20:04 -0400

    divs change color when a mouse is hovered over them

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -5,6 +5,9 @@ function createGrid() {
     for(let i=0; i<256; i++){
         const div = document.createElement("div")
         div.setAttribute("style", "aspect-ratio: 1/1; width: 6.25%")
+        div.addEventListener("mouseenter", function() {
+            this.setAttribute("class", "colored");
+        })
         body.appendChild(div)
     }
 }
