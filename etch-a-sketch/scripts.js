02f64437eaad3c39aaa864d67220f9c926787376
kayla <koala>
2026-06-12 15:35:34 -0400

    Allows the user to specify a grid size and clear the canvas

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,16 +1,32 @@
-function createGrid() {
+const gridContainer = document.getElementById("gridContainer");
 
-    const body = document.getElementById("gridContainer")
-
-    for(let i=0; i<256; i++){
-        const div = document.createElement("div")
-        div.setAttribute("style", "aspect-ratio: 1/1; width: 6.25%")
+function createGrid(size) {
+    for(let i=0; i<(size*size); i++){
+        const div = document.createElement("div");
+        div.setAttribute("style", "aspect-ratio: 1/1; width: "+100/size+"%");
+        div.setAttribute("class", "blank");
         div.addEventListener("mouseenter", function() {
             this.setAttribute("class", "colored");
         })
-        body.appendChild(div)
+        gridContainer.appendChild(div);
     }
 }
 
+function deleteGrid() {
+    while(gridContainer.hasChildNodes()){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
 
-createGrid()
+//creates initial grid
+createGrid(16);
+
+//creates a button that makes a new grid of user specified size
+const button = document.getElementById("newGrid");
+button.addEventListener("click", () => {
+    let newSize = prompt("How large would you like your grid to be? (1-100)");
+
+    
+    deleteGrid();
+    createGrid(newSize);
+})
