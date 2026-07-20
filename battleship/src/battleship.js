e911a84f2496ff50aa1d525270e1dfbac07bbfdc
kayla <koala>
2026-07-20 16:06:11 -0400

    Can now create a random board

--- battleship/src/battleship.js
+++ battleship/src/battleship.js
@@ -1,26 +1,30 @@
 export class Ship {
-    constructor(length){
-        this.length=length;
-        this.timesHit=0;
-        this.isShipSunk=false;
+    constructor(length) {
+        this.length = length;
+        this.timesHit = 0;
+        this.isShipSunk = false;
     }
 
     hit() {
-        this.timesHit+=1;
-        if(this.timesHit==this.length){
-            this.isShipSunk=true;
+        this.timesHit += 1;
+        if (this.timesHit == this.length) {
+            this.isShipSunk = true;
         }
     }
 
     isSunk() {
         return this.isShipSunk;
     }
+
+    getLength() {
+        return this.length;
+    }
 }
 
 export class Gameboard {
     constructor() {
         this.board = new Array(10);
-        for(let i=0; i<this.board.length; i++){
+        for (let i = 0; i < this.board.length; i++) {
             this.board[i] = new Array(10).fill("");
         }
     }
@@ -31,13 +35,13 @@ export class Gameboard {
 
     getAllShips() {
         const ships = [];
-        
-        for(let i=0; i<this.board.length; i++){
-            for(let j=0; j<this.board[i].length; j++){
+
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
                 const square = this.board[i][j];
                 const isShip = typeof square == "object";
-                if(isShip){
-                    if(!ships.includes(square)){ships.push(square)};
+                if (isShip) {
+                    if (!ships.includes(square)) { ships.push(square) };
                 }
             }
         }
@@ -45,40 +49,63 @@ export class Gameboard {
         return ships;
     }
 
-    placeShip(row, col, length, direction){
+    isValidPlacement(row, col, shipLength, direction) {
+            function updateCoordinates() {
+                if (direction == "U") {
+                    row--;
+                } else if (direction == "D") {
+                    row++;
+                } else if (direction == "L") {
+                    col--;
+                } else if (direction == "R") {
+                    col++;
+                };
+            }
+
+            for (let i = 0; i < shipLength; i++) {
+                const isInRange = row >= 0 && row < 10 && col >= 0 && col < 10;
+                const isValidSquare = (isInRange) && (this.isEmptySquare(row, col))
+                if (!isValidSquare) { return false }
+                updateCoordinates();
+            }
+
+            return true;
+        }
+
+    placeShip(row, col, length, direction) {
         const ship = new Ship(length);
 
         function updateCoordinates() {
-            if(direction=="U"){
+            if (direction == "U") {
                 row--;
-            } else if(direction=="D"){
+            } else if (direction == "D") {
                 row++;
-            } else if(direction=="L"){
+            } else if (direction == "L") {
                 col--;
-            } else if(direction=="R"){
+            } else if (direction == "R") {
                 col++;
             }
         }
 
-        for(let i=0; i<length; i++){
+        for (let i = 0; i < length; i++) {
             this.board[row][col] = ship;
             updateCoordinates();
         }
     }
 
-    recieveAttack(row, col){
+    recieveAttack(row, col) {
         const target = this.board[row][col];
 
-        if(target == ""){
+        if (target == "") {
             this.board[row][col] = "miss";
             return "Miss";
-        } else if(target == "miss" || target == "hit"){
+        } else if (target == "miss" || target == "hit") {
             return "Spot already hit";
         } else {
             target.hit();
             this.board[row][col] = "hit";
 
-            if(target.isSunk()){
+            if (target.isSunk()) {
                 return "Hit! Ship sunk!"
             }
 
@@ -88,9 +115,9 @@ export class Gameboard {
 
     hasAliveShips() {
         const ships = this.getAllShips();
-        
-        for(let i=0; i<ships.length; i++){
-            if(!ships[i].isSunk()){
+
+        for (let i = 0; i < ships.length; i++) {
+            if (!ships[i].isSunk()) {
                 return true;
             }
         }
@@ -100,29 +127,50 @@ export class Gameboard {
     getNumShipsLeft() {
         return this.getAllShips().length;
     }
+
+    isEmptySquare(row, col) {
+        return this.board[row][col] == "";
+    }
 }
 
-export class Player{
-    constructor(name, boardID){
+export class Player {
+    constructor(name, boardID) {
         this.name = name;
         this.board = new Gameboard();
         this.boardID = boardID;
     }
 
-    generateRandomBoard(){
-        //TODO: Make actually random
-        this.board.placeShip(0, 0, 2, "R");
+    generateRandomBoard() {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+
+        const shipLengths = [5, 4, 3, 3, 2];
+        const possibleDirections = ["U", "D", "L", "R"];
+
+        for (const shipLength of shipLengths) {
+            let row = getRandomInt(10);
+            let col = getRandomInt(10);
+            let direction = possibleDirections[getRandomInt(4)];
+            while (!this.board.isValidPlacement(row, col, shipLength, direction)) {
+                row = getRandomInt(10);
+                col = getRandomInt(10);
+                direction = possibleDirections[getRandomInt(4)]
+            }
+
+            this.board.placeShip(row, col, shipLength, direction);
+        }
     }
 
-    getBoard(){
+    getBoard() {
         return this.board;
     }
 
-    getName(){
+    getName() {
         return this.name;
     }
 
-    setName(name){
+    setName(name) {
         this.name = name;
     }
 
@@ -130,11 +178,11 @@ export class Player{
         return this.boardID;
     }
 
-    attackBoard(row, col){
+    attackBoard(row, col) {
         return this.board.recieveAttack(row, col)
     }
 
-    hasLost(){
+    hasLost() {
         return !this.board.hasAliveShips();
     }
 }
